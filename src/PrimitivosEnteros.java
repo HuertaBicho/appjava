feac204646681ed72e69fa3bdfcba402dcb91fbb
HuertaBicho <10adan.adm15@gmail.com>
2025-08-25 13:03:26 +0100

    Prueba 01

---
+++ src/PrimitivosEnteros.java
@@ -0,0 +1,10 @@
+public class PrimitivosEnteros {
+    byte numeroByte = 127;
+    short numeroShort = 32267;
+    int numeroInt = 2147483647;
+    long numeroLong = 9223372036854775807L;
+
+    float realFloat = 0.00000000015f; // 1.5e-10f
+    double realDouble = 3.4028235E39;
+
+}
---
+++ src/SentenciasBucle.java
@@ -0,0 +1,17 @@
+public class SentenciasBucle {
+    public static void main(String[] args){
+        bucle:
+        for (int i = 1; i <= 7; i++){
+            int j = 1;
+            while ( j <= 8 ) {
+                if (i == 6 || i == 7){
+                    System.out.println("Día" + i + "Descanso de fin de semana");
+                    continue bucle;
+                }
+                System.out.println("Día" + i + "Trabajando a las " + j + "Hrs.");
+                j++;
+            }
+        }
+    }
+
+}
